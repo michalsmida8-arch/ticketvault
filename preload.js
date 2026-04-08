560eb973a61e76a9517d0f4a62e14d789c9dc21e
michalsmida8-arch <michal.smida8@gmail.com>
2026-04-08 11:19:19 +0200

    v3.1.1 fix Gmail sync

--- preload.js
+++ preload.js
@@ -1,6 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron')
-
 contextBridge.exposeInMainWorld('electronAPI', {
   onUpdateStatus: (callback) => ipcRenderer.on('update-status', (_, data) => callback(data)),
-  installUpdate: () => ipcRenderer.send('install-update')
+  installUpdate: () => ipcRenderer.send('install-update'),
+  fetchViagogo: (url) => ipcRenderer.invoke('fetch-viagogo', url),
+  checkGmailNow: () => ipcRenderer.invoke('check-gmail-now'),
+  onGmailUpdate: (callback) => ipcRenderer.on('gmail-update', (_, data) => callback(data)),
+  onGmailChecking: (callback) => ipcRenderer.on('gmail-checking', (_, data) => callback(data)),
+  onGmailRefresh: (callback) => ipcRenderer.on('gmail-refresh', (_, data) => callback(data)),
 })
